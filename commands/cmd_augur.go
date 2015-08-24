1fb90f9918f0391fc33a0162e602ae663a9786a2
Carlos Cobo <toqueteos@gmail.com>
2015-08-24 17:26:02 +0200

    Use Augur prefixed models.

--- commands/cmd_augur.go
+++ commands/cmd_augur.go
@@ -20,8 +20,8 @@ type CmdAugur struct {
 	// Source   string `short:"" long:"source" default:"people" description:""`
 
 	client       *readers.AugurInsightsAPI
-	emailStore   *social.EmailStore
-	insightStore *social.InsightStore
+	emailStore   *social.AugurEmailStore
+	insightStore *social.AugurInsightStore
 }
 
 func (cmd *CmdAugur) Execute(args []string) error {
@@ -43,7 +43,7 @@ func (cmd *CmdAugur) process() {
 	q := cmd.emailStore.Query()
 	q.FindWithoutStatus()
 	if cmd.FilterBy != 0 {
-		q.AddCriteria(op.Eq(social.Schema.Email.Status, cmd.FilterBy))
+		q.AddCriteria(op.Eq(social.Schema.AugurEmail.Status, cmd.FilterBy))
 	}
 
 	set := cmd.emailStore.MustFind(q)
@@ -60,7 +60,7 @@ func (cmd *CmdAugur) process() {
 	}
 }
 
-func (cmd *CmdAugur) processEmail(e *social.Email) error {
+func (cmd *CmdAugur) processEmail(e *social.AugurEmail) error {
 	insight, resp, err := cmd.client.SearchByEmail(e.Email)
 	if err != nil && resp == nil {
 		return err
@@ -76,7 +76,7 @@ func (cmd *CmdAugur) processEmail(e *social.Email) error {
 	return err
 }
 
-func (cmd *CmdAugur) setStatus(doc *social.Email, status int) error {
+func (cmd *CmdAugur) setStatus(doc *social.AugurEmail, status int) error {
 	doc.Status = status
 	doc.Last = time.Now()
 
@@ -85,6 +85,6 @@ func (cmd *CmdAugur) setStatus(doc *social.Email, status int) error {
 	return err
 }
 
-func (cmd *CmdAugur) saveAugurInsights(doc *social.Insight) error {
+func (cmd *CmdAugur) saveAugurInsights(doc *social.AugurInsight) error {
 	return cmd.insightStore.Insert(doc)
 }
